b8f10bbda107ff57421e8662352b012969c284a2
suuryabks <kmechyderabad@gmail.com>
2022-06-21 11:22:33 +0000

    Code Synced by DhiWise

---
+++ src/components/RatingBar/index.js
@@ -0,0 +1,31 @@
+import { ErrorMessage } from "components/ErrorMessage";
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+
+export const RatingBar = ({
+  children,
+  className,
+  starCount = 5,
+  color = "grey",
+  activeColor = "red",
+  isEditable = false,
+  errors = [],
+  ...restProps
+}) => {
+  return (
+    <>
+      <ReactStars
+        edit={isEditable}
+        classNames={className}
+        count={starCount}
+        isHalf={false}
+        color={color}
+        activeColor={activeColor}
+        {...restProps}
+        key={restProps.value || 1}
+      />
+      <ErrorMessage errors={errors} />
+      {children}
+    </>
+  );
+};
